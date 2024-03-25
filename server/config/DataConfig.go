ce7e30d97bde33e39abe7889a3fcc8c4fdd8bc65
zhengwei.zeng@ui.com <zhengwei.zeng@ui.com>
2024-03-25 16:40:38 +0800

    config host port

--- server/config/DataConfig.go
+++ server/config/DataConfig.go
@@ -87,7 +87,7 @@ const (
 
 	//mysql服务配置信息 root:root 设置mysql账户的用户名和密码
 
-	MysqlDsn = "root:root@(172.28.40.34:3306)/film?charset=utf8mb4&parseTime=True&loc=Local"
+	MysqlDsn = "root:root@(127.0.0.1:3306)/film?charset=utf8mb4&parseTime=True&loc=Local"
 
 	// MysqlDsn docker compose 环境下的链接信息 mysql:3306 为 docker compose 中 mysql服务对应的网络名称和端口
 	//MysqlDsn = "root:root@(mysql:3306)/FilmSite?charset=utf8mb4&parseTime=True&loc=Local"
@@ -98,7 +98,7 @@ const (
 		RedisPassword redis访问密码
 		RedisDBNo 使用第几号库
 	*/
-	RedisAddr     = "172.28.40.34:6379"
+	RedisAddr     = "127.0.0.1:6379"
 	RedisPassword = "2333tv.top"
 	RedisDBNo     = 0
 
